aa5984f4555ce4b13d7be7e0f10189a26a9dd6f4
Magnus Kulke <mkulke@gmail.com>
2018-12-21 23:00:19 +0100

    wip

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -15,14 +15,16 @@ use std::time::{Duration, Instant};
 
 use rand::distributions::{IndependentSample, Range};
 
-use tokio::timer::Delay;
+// use tokio::timer::Delay;
+use tokio::timer::Interval;
 
 use failure::Error;
 
 use futures::future::lazy;
-use futures::sync::mpsc;
-use futures::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+// use futures::sync::mpsc;
+// use futures::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 use futures::{Future, Stream};
+// use futures::Future;
 
 use grpcio::{ChannelBuilder, EnvBuilder};
 use grpcio::{Environment, RpcContext, RpcStatus, RpcStatusCode, Server, ServerBuilder, UnarySink};
@@ -46,10 +48,6 @@ struct Config {
 enum VotedFor {
     Candidate(SocketAddrV4),
     NoOne,
-}
-
-enum Message {
-    VotedFor(SocketAddrV4),
 }
 
 fn get_cli_app<'a, 'b>() -> App<'a, 'b> {
@@ -80,13 +78,11 @@ struct LeaderElectionService {
     config: Config,
     current_term: u64,
     voted_for: VotedFor,
-    tx: UnboundedSender<Message>,
 }
 
 impl LeaderElectionService {
-    fn new(tx: UnboundedSender<Message>, config: Config) -> LeaderElectionService {
+    fn new(config: Config) -> LeaderElectionService {
         LeaderElectionService {
-            tx: tx,
             config: config,
             current_term: 0,
             voted_for: VotedFor::NoOne,
@@ -99,22 +95,6 @@ impl LeaderElectionService {
             .iter()
             .find(|member| format!("{}:{}", member.ip(), member.port()) == candidate)
     }
-
-    fn vote_yes(
-        &self,
-        member: &SocketAddrV4,
-        sink: UnarySink<VoteReply>,
-    ) -> Box<Future<Item = (), Error = grpcio::Error> + Send> {
-        let mut rep = VoteReply::new();
-        let tx = self.tx.clone();
-        let mgns = VotedFor::Candidate(*member);
-        // self.voted_for
-        rep.set_yes(true);
-        let message = Message::VotedFor(*member);
-        Box::new(sink.success(rep.clone()).inspect(move |_| {
-            let _ = tx.unbounded_send(message);
-        }))
-    }
 }
 
 impl LeaderElection for LeaderElectionService {
@@ -124,32 +104,25 @@ impl LeaderElection for LeaderElectionService {
         req: VoteRequest,
         sink: UnarySink<VoteReply>,
     ) -> () {
-        let mut rep = VoteReply::new();
-        let voted_for = &self.voted_for;
-        let fut = match voted_for {
-            VotedFor::Candidate(_) => {
-                rep.set_yes(false);
-                Box::new(sink.success(rep.clone()))
-            }
-            VotedFor::NoOne => {
-                let member = self.to_member(req.candidate);
-                match member {
-                    None => sink
-                        .fail(RpcStatus::new(
-                            RpcStatusCode::InvalidArgument,
-                            Some("candidate not in member list".to_string()),
-                        ))
-                        .boxed(),
-                    // Some(m) => self.vote_yes(m, sink),
-                    Some(m) => {
-                        // let mgns = VotedFor::Candidate(*m);
-                        // self.voted_for = m;
-                        sink.success(rep.clone())
-                            .inspect(move |_| {
-                                let message = Message::VotedFor(*m);
-                                let _ = self.tx.unbounded_send(message);
-                            })
-                            .boxed()
+        let fut = match self.to_member(req.candidate) {
+            None => Box::new(sink.fail(RpcStatus::new(
+                RpcStatusCode::InvalidArgument,
+                Some("candidate not in member list".to_string()),
+            ))),
+            Some(&member) => {
+                let mut rep = VoteReply::new();
+                let voted_for = &self.voted_for;
+                match voted_for {
+                    VotedFor::Candidate(_) => {
+                        rep.set_yes(false);
+                        println!("rejected vote for {}", member);
+                        Box::new(sink.success(rep.clone()))
+                    }
+                    VotedFor::NoOne => {
+                        rep.set_yes(true);
+                        self.voted_for = VotedFor::Candidate(member);
+                        println!("voted for {}", member);
+                        Box::new(sink.success(rep.clone()))
                     }
                 }
             }
@@ -158,11 +131,11 @@ impl LeaderElection for LeaderElectionService {
     }
 }
 
-fn start_server(config: Config, tx: UnboundedSender<Message>) -> Result<Server, Error> {
+fn start_server(config: Config) -> Result<Server, Error> {
     let env = Arc::new(Environment::new(1));
     let ip = config.listen.ip().to_string();
     let port = config.listen.port();
-    let service = LeaderElectionService::new(tx, config);
+    let service = LeaderElectionService::new(config);
     let grpc_service = raft_grpc::create_leader_election(service);
     let mut server = ServerBuilder::new(env)
         .register_service(grpc_service)
@@ -188,19 +161,20 @@ fn get_raft_client(addr: &SocketAddrV4) -> LeaderElectionClient {
     LeaderElectionClient::new(ch)
 }
 
-fn request_vote(addr: &SocketAddrV4) -> impl Future<Item = bool, Error = ()> {
+fn request_vote(client: LeaderElectionClient, addr: &SocketAddrV4) -> impl Future<Item = bool, Error = ()> {
     use futures::future::{ok, Either};
 
-    let client = get_raft_client(addr);
+    // let client = get_raft_client(addr);
     let mut req = VoteRequest::new();
     req.set_term(1);
-    let res = client.request_vote_async(&req);
-    let ip = *addr.ip();
+    req.set_candidate(format!("{}:{}", addr.ip(), addr.port()));
+    let res = client.request_vote_async(&req); 
+    let a = addr.clone();
     match res {
         Ok(y) => Either::A(
             y.map(|z| z.get_yes())
                 .map_err(move |e| {
-                    eprintln!("Failed to send request to {:?}: {:?}", ip, e);
+                    eprintln!("Failed to send vote request to {:?} {:?}", a, e);
                     ()
                 })
                 .or_else(|_| Ok(false)),
@@ -216,23 +190,22 @@ fn get_random_duration() -> Duration {
     Duration::new(sample, 0)
 }
 
-fn do_business(config: Config, rx: UnboundedReceiver<Message>) -> () {
+fn do_business(config: Config) -> () {
     tokio::run(lazy(move || {
-        config.cluster.iter().for_each(|addr| {
-            let now = Instant::now();
-            let duration = get_random_duration();
-            let delay = Delay::new(now + duration).map_err(|_| ());
-            let vote = request_vote(addr);
-            tokio::spawn(delay.and_then(|_| vote).map(|_| ()));
-        });
-        rx.for_each(|message| {
-            match message {
-                Message::VotedFor(candidate) => {
-                    println!("voted for {}", candidate);
-                }
-            }
-            Ok(())
-        })
+        let now = Instant::now();
+        let duration = get_random_duration();
+        let interval = Interval::new(now, duration)
+            .for_each(move |_| {
+                config.cluster.iter().for_each(|addr| {
+                    let client = get_raft_client(addr);
+                    let vote = request_vote(client, &config.listen).map(|_| ());
+                    tokio::spawn(vote);
+                });
+                Ok(())
+            })
+            .map_err(|_| ());
+        tokio::spawn(interval);
+        Ok(())
     }));
 }
 
@@ -242,13 +215,13 @@ fn main() -> () {
     let listen = value_t!(matches, "listen", SocketAddrV4).unwrap();
     let cluster = values_t!(matches, "cluster", SocketAddrV4).unwrap();
     let config = Config { listen, cluster };
-    let (tx, rx) = mpsc::unbounded();
-    match start_server(config.clone(), tx) {
+    // let (tx, rx) = mpsc::unbounded();
+    match start_server(config.clone()) {
         Err(ref err) => {
             bail_out(err);
         }
         Ok(_) => {
-            let _ = do_business(config, rx);
+            let _ = do_business(config);
         }
     }
 }
